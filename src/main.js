f09e09e3e1cfc34d391495aa64975ec0441b50a5
Sergey Morozov <morozov.sergey@gmail.com>
2022-09-25 13:13:40 -0700

    example vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 import router from './router'
 
 import './assets/base.css'
 
+const store = createStore({
+  state () {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,22 @@
-<script setup>
+<script>
+export default {
   // https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
   // https://cdnjs.cloudflare.com/ajax/libs/jmespath/0.15.0/jmespath.min.js
   // https://cdn.jsdelivr.net/npm/chart.js@2.8.0
-  // https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js
-  // https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js
+
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+      console.log(this.$store.state.count)
+    }
+  }
+}
 </script>
 
 <template>
   <main class="ui">
-    HOME
+    HOME {{ $store.state.count }}
+
+    <button @click="increment">incrment</button>
   </main>
 </template>
